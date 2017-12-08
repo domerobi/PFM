5b0349461269b1bdb96271d5caef151afb210b0c
Róbert Dömötör <domerobi2@gmail.com>
2017-12-09 00:13:15 +0100

    Deleted unnecessary method

--- PFM/MainWindow.xaml.cs
+++ PFM/MainWindow.xaml.cs
@@ -30,65 +30,10 @@ namespace PFM
             FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(
                         XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
             InitializeComponent();
-            dpDate.Text = DateTime.Now.ToString();
             DataContext = new MainViewModel();
         }
 
         #endregion
-        
-        #region Event handlers
-
-        /// <summary>
-        /// Add new item to database and ViewModel list
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void btnSubmit_Click(object sender, RoutedEventArgs e)
-        {
-            // Insert new item to database
-            //con.Open();
-
-            //SqlCommand cmd = con.CreateCommand();
-            //cmd.CommandType = CommandType.Text;
-            //cmd.CommandText = "INSERT INTO dbo.Inventory (Type, Category, Sum, Date, Comment)" +
-            //                  "VALUES(@type, @category, @sum, @date, @comment)";
-
-            //cmd.Parameters.AddWithValue("@type", ddType.Text);
-            //cmd.Parameters.AddWithValue("@category", ddCategory.Text);
-            //cmd.Parameters.AddWithValue("@sum", Convert.ToInt32(tbSum.Text));
-            //cmd.Parameters.AddWithValue("@date", Convert.ToDateTime(dpDate.Text));
-            //cmd.Parameters.AddWithValue("@comment", tbComment.Text.ToString());
-
-            //cmd.ExecuteNonQuery();
-
-            //con.Close();
-            
-            //// Insert new item to ViewModel list
-            //MainViewModel vm = (MainViewModel)this.DataContext;
-            //vm.DBInventory.InventoryRecords.Add(new Inventory
-            //{
-            //    Type = ddType.Text,
-            //    Category = ddCategory.Text,
-            //    Sum = Convert.ToInt32(tbSum.Text),
-            //    Date = Convert.ToDateTime(dpDate.Text),
-            //    Comment = tbComment.Text
-            //});
-            //// Sort list by date in descending order
-            //vm.DBInventory.InventoryRecords = vm.DBInventory.InventoryRecords.OrderByDescending(record => record.Date).ToList();
-
-            //// Bind ItemsSource to the updated list
-            //dgInventory.ItemsSource = null;
-            //dgInventory.ItemsSource = vm.DBInventory.InventoryRecords;
-
-            //// Clear the input fields
-            //vm.ItemType.SelectedItemType = vm.ItemType.Types[0]; ;
-            //tbSum.Text = "";
-            //dpDate.Text = DateTime.Now.ToString();
-            //tbComment.Text = "";
-        }
-
-        #endregion
-        
     }
 
     #region Converter
